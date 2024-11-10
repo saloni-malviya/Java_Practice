4371ee5d96971d7d0cf823d9fab0d64a435900f1
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-11-10 22:16:27 +0530

    Add files via upload

---
+++ Factor_of_a_number.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//.......Program to find factor of a number......................
+package practice_question_java;
+
+
+public class Factor_of_a_number {
+    public static void main(String[]args)
+    {
+        int num=12;
+        for(int i=1;i<=num;i++)
+        {
+        if(num%i==0)
+        {
+            System.out.print(i+" ");
+            
+        }
+    }
+    }
+}
