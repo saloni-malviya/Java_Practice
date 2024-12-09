4477a7dab9945f5562a996ff75fa9075335332be
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-12-09 22:01:24 +0530

    Add files via upload

---
+++ Power_of_a_number.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//........Program to print power of a given number..............
+package practice_question_java;
+
+public class Power_of_a_number {
+    public static void main(String[]args)
+    {
+        //int result=1;
+        int base=2;
+        int expo=3;
+        double result=Math.pow(base,expo);
+        System.out.println(result);
+    }
+    
+}
